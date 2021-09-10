731ab5fe4781cee0fa7a54cf63dfc45a617a7046
Patrick Hall <ptrinkz@gmail.com>
2021-09-10 11:08:20 -0400

    banana

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+        System.out.print("DOnkey Konga bestr agamne");
+        System.out.println(" DOnket Konga is Beskt gakess since Dongket kongaas");
+    }
+}
