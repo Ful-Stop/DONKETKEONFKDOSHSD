a5bedf5b5e9f0bc5b9a22aa24bb4e3775dd6674c
Patrick Hall <ptrinkz@gmail.com>
2021-09-10 11:35:01 -0400

    dirt

---
+++ src/com/company/Yummyfood.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Yummyfood {
+    public static void main(String[] args) {
+        System.out.print("yummy ");
+        System.out.println("mario");
+        chocolate();
+        chocolate();
+        iceCream();
+        System.out.println("gonna go the the pizza hut combination taco bell");
+    }
+    public static void chocolate(){
+        System.out.println("chococlates");
+    }
+    public static void iceCream(){
+        System.out.println("Donkey Kong Flavored");
+        chocolate();
+        System.out.println("tastes like asphalt");
+    }
+}
